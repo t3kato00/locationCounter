dc2d1bcf7f4e422c5f84fe079e993cc33c61f898
unknown <t3vato00@TYOD-ATK3-22.kk.lan>
2014-09-24 13:02:46 +0300

    GeoMath.js added.

---
+++ www/js/geoMath.js
@@ -0,0 +1,14 @@
+function deg2rad(deg) {
+		return deg * (Math.PI/180);
+	}
+	function calculateDistance(coordsA, coordsB) {
+		var dLat = deg2rad(coordsA.latitude-coordsB.latitude);
+		var dLon = deg2rad(coordsA.longitude-coordsB.longitude);
+		var a =
+			Math.sin(dLat/2) * Math.sin(dLat/2) +
+			Math.cos(deg2rad(coordsB.latitude)) * Math.cos(deg2rad(coordsA.latitude)) *
+			Math.sin(dLon/2) * Math.sin(dLon/2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		var d = 6371000 * c;
+		return d;
+	}
