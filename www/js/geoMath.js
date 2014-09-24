d4f19f06752fd7456a097d6561c555010ce981a0
Topi Karvonen <topi.karvonen@gmail.com>
2014-09-24 13:07:56 +0300

    Cleaned up geoMath.js

--- www/js/geoMath.js
+++ www/js/geoMath.js
@@ -1,14 +1,16 @@
+
 function deg2rad(deg) {
-		return deg * (Math.PI/180);
-	}
-	function calculateDistance(coordsA, coordsB) {
-		var dLat = deg2rad(coordsA.latitude-coordsB.latitude);
-		var dLon = deg2rad(coordsA.longitude-coordsB.longitude);
-		var a =
-			Math.sin(dLat/2) * Math.sin(dLat/2) +
-			Math.cos(deg2rad(coordsB.latitude)) * Math.cos(deg2rad(coordsA.latitude)) *
-			Math.sin(dLon/2) * Math.sin(dLon/2);
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		var d = 6371000 * c;
-		return d;
-	}
+	return deg * (Math.PI/180);
+}
+
+function calculateDistance(coordsA, coordsB) {
+	var dLat = deg2rad(coordsA.latitude-coordsB.latitude);
+	var dLon = deg2rad(coordsA.longitude-coordsB.longitude);
+	var a =
+		Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(deg2rad(coordsB.latitude)) * Math.cos(deg2rad(coordsA.latitude)) *
+		Math.sin(dLon/2) * Math.sin(dLon/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	var d = 6371000 * c;
+	return d;
+}
