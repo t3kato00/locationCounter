3a6f568d2471856ac81451d1157b6103ab6c85a3
Topi Karvonen <topi.karvonen@gmail.com>
2014-10-01 10:51:41 +0300

    Canoninalized javascript.

--- www/js/index.js
+++ www/js/index.js
@@ -16,22 +16,27 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-    }
-};
+var app =
+	// Application Constructor
+	{ initialize: function()
+		{
+			this.bindEvents();
+		}
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	, bindEvents: function()
+		{
+			document.addEventListener('deviceready', this.onDeviceReady, false);
+		}
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	, onDeviceReady: function()
+		{
+			alert("Ready!");
+		}
+	};
+
