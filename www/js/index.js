fe85d08b33d6dd75df4f349b83995e7dd64508ea
Topi Karvonen <topi.karvonen@gmail.com>
2014-10-06 14:22:13 +0300

    Implemented initial place modification functionality.

--- www/js/index.js
+++ www/js/index.js
@@ -42,15 +42,16 @@ var app =
 				return false;
 			};
 			app.setPosition('Pending', 'pending');
-			app.setTabs();
-			app.tabs[app.activeTab]();
+			app.refreshTabs();
+			app.refreshActiveTab();
+			app.db = { nextId: 0, places: {} };
 		}
 	, setPosition: function( inside, style )
 		{
 			var elem = $('#position');
 			if( !inside )
 			{
-				elem.slideUp( function() { elem.html('') } );
+				elem.slideUp( function() { elem.html(''); } );
 				return;
 			}
 
@@ -66,39 +67,50 @@ var app =
 
 			elem.html(inside).slideDown();
 		}
-	, handlerOnTabClick: function(name)
+	, handlerOnTabClick: function(n)
 		{	return function()
 			{
-				if( app.activeTab == name )
+				if( app.activeTab == n )
 					return;
 
-				app.activeTab = name;
-				app.setTabs();
-				app.tabs[name]();
-			}
+				app.activeTab = n;
+				app.refreshTabs();
+				app.tabs[n].show();
+			};
 		}
-	// Refresh tabs.
-	, setTabs: function()
+	, refreshActiveTab: function()
+		{
+			app.tabs[app.activeTab].show();
+		}
+	, renameActiveTab: function(name)
+		{
+			app.tabs[app.activeTab].name = name;
+			app.refreshTabs();
+		}
+	, newTab: function(tab,setActive)
+		{
+			app.tabs.push(tab);
+			if(setActive)
+				app.activeTab = app.tabs.length-1;
+			app.refreshTabs();
+			if(setActive)
+				app.refreshActiveTab();
+		}
+	, refreshTabs: function()
 		{
 			var content = '';
-			var shows = {};
-			var count = 0;
+			var shows = { };
 
-			for( var key in app.tabs ) {
-				var n = count;
-				count += 1;
-				var id = 'tabSwitch_' + n;
-				shows[id] = app.handlerOnTabClick(key);
+			if( app.tabs.length > 1 )
+				for( var count = 0; count < app.tabs.length; count += 1 ) {
+					var n = count;
+					var id = 'tabSwitch_' + n;
+					shows[id] = app.handlerOnTabClick(n);
 
-				var evenOdd = 'Even';
-				if( n % 2 ) evenOdd = 'Odd';
-
-				if( key == app.activeTab ) {
-					content += '<a id="' + id + '" class="tabSwitcher tabSwitcher' + evenOdd + ' activeTab">' + key + '</span>'
-				} else {
-					content += '<a id="' + id + '" class="tabSwitcher tabSwitcher' + evenOdd + '">' + key + '</span>'
+					var evenOdd = n % 2 ? 'Even' : 'Odd';
+					var activeTab = n == app.activeTab ? " activeTab" : "";
+					content += '<a id="' + id + '" class="tabSwitcher tabSwitcher' + evenOdd + activeTab + '">' + app.tabs[n].name + '</span>';
 				}
-			}
 
 			$('#tabs').html(content);
 			for( var id in shows ) {
@@ -107,78 +119,122 @@ var app =
 		}
 	, setMain: function( inside )
 		{
-			//document.getElementById("main").innerHTML = inside;
 			$('#main').html(inside);
 		}
 	, tabs:
-		{ "Add place": function()
-			{
-				var content =
-					'<table class="form">' +
-					'<tr><td>Name:</td><td><input type="text" id="apName"></td></tr>' +
-					'<tr><td>Radius:</td><td><input type="text" id="apRadius"></td></tr>' +
-					'</table>' +
-					'<button type="button" id="apSubmit">Submit</button>';
-				app.setMain( content );
-				$('#apSubmit').on("click", function () {
-					var name = document.getElementById("apName").value;
-					var radius = parseFloat(document.getElementById("apRadius").value);
-					if(isNaN(radius)) {
-						alert('Invalid radius!');
-						return;
+		[	{ name: "Statistics"
+			, show: function()
+				{
+					var contents = '';
+					contents += '<button type="button" id="stNew">New place</button>';
+
+					app.setMain(contents);
+
+					$('#stNew').on("click", function() {
+						app.newTab(app.addModifyPlaceTab({}),true);
+					});
+				}
+			}
+		,	{ name: "Debug"
+			, show: function()
+				{
+					var sect = function(text) {
+						return '<h2>' + text + '</h2>';
 					}
-				});
-			}
-		, "Statistics": function()
-			{
-				app.setMain( "Statistics" );
-			}
-		, "Debug": function()
-			{
-				var sect = function(text) {
-					return '<h2>' + text + '</h2>';
+					var head = function(text) {
+						return '<h3>' + text + '</h3>';
+					}
+					var coords = function(coords) {
+						var result = '<table>' +
+							'<tr><td>Latitude</td><td>' + geoMath.rad2deg(coords.latitude) + '</td></tr>' +
+							'<tr><td>Longitude</td><td>' + geoMath.rad2deg(coords.longitude) + '</td></tr>';
+
+						if('accuracy' in coords)
+							result += '<tr><td>Radius</td><td>' + coords.accurary + '</td></tr>';
+						else if('rangle' in coords)
+							result += '<tr><td>Radius</td><td>' + geoMath.rangle2meters(coords.rangle) + '</td></tr>';
+
+						result += '</table>';
+						return result;
+					}
+					var contents = "";
+
+					//if(false)
+					contents +=
+						sect("GeoMath") +
+						head("Test1") + coords(geoMath.nvect2coords(geoMath.coords2nvect({latitude: Math.PI/4, longitude: Math.PI/8}))) +
+						head("Test2") + coords(geoMath.placePlus({latitude:0, longitude:0, rangle: 0},{latitude:Math.PI/2, longitude:0, rangle: 0})) +
+						head("Test3") + coords(geoMath.placePlus({latitude:0, longitude:0, rangle: 0},{latitude: geoMath.meters2rangle(2000), longitude:0, rangle: 0})) +
+						head("Test4") + geoMath.distance({latitude:0, longitude:0},{latitude: geoMath.meters2rangle(2000), longitude:0}) + "<br>Should be 2000." +
+						head("Test5") + coords(geoMath.nvect2coords([0,0,1])) +
+						head("Test6") + coords(geoMath.nvect2coords([0,0,-1]));
+
+					contents +=
+						sect("Position") +
+						head("Test") + '<a id="normalPos">normal</a> <a id="errorPos">error</a> <a id="pendingPos">pending</a> <a id="foundPos">found</a> <a id="clearPos">clear</a>';
+
+					app.setMain( contents );
+					$('#normalPos').on("click", function() { app.setPosition('Test') });
+					$('#errorPos').on("click", function() { app.setPosition('Error', 'error') });
+					$('#pendingPos').on("click", function() { app.setPosition('Pending', 'pending') });
+					$('#foundPos').on("click", function() { app.setPosition('Found', 'found') });
+					$('#clearPos').on("click", function() { app.setPosition() });
 				}
-				var head = function(text) {
-					return '<h3>' + text + '</h3>';
-				}
-				var coords = function(coords) {
-					var result = '<table>' +
-						'<tr><td>Latitude</td><td>' + geoMath.rad2deg(coords.latitude) + '</td></tr>' +
-						'<tr><td>Longitude</td><td>' + geoMath.rad2deg(coords.longitude) + '</td></tr>';
-
-					if('accuracy' in coords)
-						result += '<tr><td>Radius</td><td>' + coords.accurary + '</td></tr>';
-					else if('rangle' in coords)
-						result += '<tr><td>Radius</td><td>' + geoMath.rangle2meters(coords.rangle) + '</td></tr>';
-
-					result += '</table>';
-					return result;
-				}
-				var contents = "";
-
-				//if(false)
-				contents +=
-					sect("GeoMath") +
-					head("Test1") + coords(geoMath.nvect2coords(geoMath.coords2nvect({latitude: Math.PI/4, longitude: Math.PI/8}))) +
-					head("Test2") + coords(geoMath.placePlus({latitude:0, longitude:0, rangle: 0},{latitude:Math.PI/2, longitude:0, rangle: 0})) +
-					head("Test3") + coords(geoMath.placePlus({latitude:0, longitude:0, rangle: 0},{latitude: geoMath.meters2rangle(2000), longitude:0, rangle: 0})) +
-					head("Test4") + geoMath.distance({latitude:0, longitude:0},{latitude: geoMath.meters2rangle(2000), longitude:0}) + "<br>Should be 2000." +
-					head("Test5") + coords(geoMath.nvect2coords([0,0,1])) +
-					head("Test6") + coords(geoMath.nvect2coords([0,0,-1]));
-
-				contents +=
-					sect("Position") +
-					head("Test") + '<a id="normalPos">normal</a> <a id="errorPos">error</a> <a id="pendingPos">pending</a> <a id="foundPos">found</a> <a id="clearPos">clear</a>';
-
-				app.setMain( contents );
-				$('#normalPos').on("click", function() { app.setPosition('Test') });
-				$('#errorPos').on("click", function() { app.setPosition('Error', 'error') });
-				$('#pendingPos').on("click", function() { app.setPosition('Pending', 'pending') });
-				$('#foundPos').on("click", function() { app.setPosition('Found', 'found') });
-				$('#clearPos').on("click", function() { app.setPosition() });
 			}
-		}
+		]
 	// Currently active tab.
-	, activeTab: "Statistics"
+	, activeTab: 0
+	, coordinateTable: function(coords) {
+			var result =
+				'<tr><td>Latitude</td><td>' + geoMath.rad2deg(coords.latitude) + '</td></tr>' +
+				'<tr><td>Longitude</td><td>' + geoMath.rad2deg(coords.longitude) + '</td></tr>';
+
+			if('accuracy' in coords)
+				result += '<tr><td>Radius</td><td>' + coords.accurary + '</td></tr>';
+			else if('rangle' in coords)
+				result += '<tr><td>Radius</td><td>' + geoMath.rangle2meters(coords.rangle) + '</td></tr>';
+
+			return result;
+		}
+	, addModifyPlaceTab: function(place)
+		{
+			return { name: place.name, show: function()
+				{
+					var contents = '<table>';
+
+					contents += '<tr><td>Name</td><td><input type="text" id="amptName"></td></tr>';
+					if( place.area )
+						contents += app.coordinateTable(place.area);
+					contents += '</table>';
+					contents += '<button type="button" id="amptExpand">Expand Area</button>';
+
+					app.setMain(contents);
+
+					$('#amptName').on("input", function() {
+					  	var name = $('#amptName').val();
+						if(! place.id )
+						{
+							place.id = app.db.nextId;
+							app.db.nextId += 1;
+							app.db.places[place.id] = place;
+						}
+						place.name = name;
+						app.renameActiveTab(name);
+					});
+					$('#amptExpand').on("click", function() {
+						if( app.currentArea )
+						{
+							if( place.area )
+								place.area = geoMath.placePlus(place.area,app.currentArea);
+							else
+								place.area = geoMath.currentArea;
+							app.refreshActiveTab();
+						}
+						else
+							alert("No location information currently available!");
+					});
+				}
+			};
+		}
 	};
 
